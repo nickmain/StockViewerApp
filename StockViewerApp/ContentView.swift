c90b89117b090bb997ff8c7fbeef6473371e2da8
Nick Main <david.nick.main@gmail.com>
2024-03-01 12:26:02 -0800

    Initial Commit

---
+++ StockViewerApp/ContentView.swift
@@ -0,0 +1,19 @@
+// Copyright (c) 2024 David N Main
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockViewerApp/StockViewerAppApp.swift
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 David N Main
+
+import SwiftUI
+
+@main
+struct StockViewerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
